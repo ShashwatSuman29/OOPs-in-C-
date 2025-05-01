83a8f5773de267857b39c6166705d1bdc904d88b
Shashwat Suman <shashwatsumanat29@gmail.com>
2025-05-02 00:12:49 +0530

    car user defined data type

---
+++ carUserDefinedDataType.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Car {
+public:    
+    string name;
+    int price;
+    int seaters;
+    string type;
+};
+
+int main (){
+    Car c1;
+    c1.name = "BMW";
+    c1.price = 1000000;
+    c1.seaters = 5;
+    c1.type = "Luxury";
+
+    Car c2;
+    c2.name = "Tata";
+    c2.price = 500000;
+    c2.seaters = 5;
+    c2.type = "Budget";
+
+
+    cout<<c1.name<<" "<<c1.price<<" "<<c1.seaters<<" "<<c1.type<< endl;
+    cout<<c2.name<<" "<<c2.price<<" "<<c2.seaters<<" "<<c2.type<< endl;
+};
