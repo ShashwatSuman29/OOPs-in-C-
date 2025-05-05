7848f44ef93be10ff68d125aa65af3454e5b0232
Shashwat Suman <shashwatsumanat29@gmail.com>
2025-05-05 20:39:26 +0530

    this keyword

--- ThisKeyword.cpp
+++ ThisKeyword.cpp
@@ -13,16 +13,21 @@ public:
         this->runs = runs;
         this->average = average;
     }
-    // void print(Cricketer c)
-    // {
-    //     cout << c.name << " " << c.runs << " " << c.average << endl;
-    // }
+  
 };
+
+void print(Cricketer c)
+{
+    cout << c.name << " " << c.runs << " " << c.average << endl;
+}
 int main()
 {
     Cricketer c1("Sachin", 10000, 50);
     Cricketer c2("Dravid", 5000, 40);
 
-    cout << c1.name << " " << c1.runs << " " << c1.average << endl;
-    cout << c2.name << " " << c2.runs << " " << c2.average << endl;
+    // cout << c1.name << " " << c1.runs << " " << c1.average << endl;
+    // cout << c2.name << " " << c2.runs << " " << c2.average << endl;
+
+    print(c1);
+    print(c2);
 };
